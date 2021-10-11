6d88125d88667148ac9baba8e5b69064de9dacdb
quangkhoinguyen1995@gmail.com <quangkhoinguyen1995@gmail.com>
2021-10-11 17:46:28 +1300

    Almost done with DiaryWindow

--- DiaryWindow.xaml.cs
+++ DiaryWindow.xaml.cs
@@ -42,15 +42,64 @@ namespace language_learning_tracker
         {
             LanguageActivity activity = new LanguageActivity();
             activity.ActivityDate = Activity_Date.SelectedDate.GetValueOrDefault();
-            activity.ActivityType = Activity_Type.SelectedItem.ToString();
+            
             if (Language_Name.SelectedIndex == -1)
+            {
                 MessageBox.Show("Please select a language!");
+                return;
+            }
             else
             {
                 string selectedLanguage = Language_Name.SelectedItem.ToString();
                 activity.LanguageID = LanguageDataContext.Language.Where(l => l.LanguageName == selectedLanguage).Select(p => p.LanguageID).Single();
             }
-            
+
+            if (Activity_Type.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an activity");
+                return;
+            }
+            else
+                activity.ActivityType = Activity_Type.SelectedItem.ToString();
+
+            if (Media_Name.Text.Length == 0 || Media_Type.Text.Length == 0 || Time_taken.Text.Length == 0)
+            {
+                MessageBox.Show("Please fill out all the info!");
+                return;
+            }
+
+            TimeSpan timeTaken = TimeSpan.FromHours(Double.Parse(Time_taken.Text));
+            activity.TimeTaken = timeTaken;
+            ImmersionMedia iMedia = CheckOrCreateMedia(Media_Name.Text, Media_Type.Text, activity.LanguageID, timeTaken);
+            activity.MediaID = iMedia.MediaID;
+            LanguageDataContext.Add(activity);
+            LanguageDataContext.SaveChanges();
+            MessageBox.Show("Activity sucessfully added!");
+        }
+
+        private ImmersionMedia CheckOrCreateMedia (string Media_Name, string Media_Type, int LanguageID, TimeSpan timeTaken)
+        {
+            int MediaIDQuery = LanguageDataContext.ImmersionMediaDB.Where(
+                m => m.MediaName == Media_Name && m.MediaType == Media_Type && m.LanguageID == LanguageID).Select(
+                m => m.MediaID).FirstOrDefault();
+
+            if (MediaIDQuery != 0)
+            {
+                ImmersionMedia requestedMedia = LanguageDataContext.ImmersionMediaDB.Where(
+                    m => m.MediaName == Media_Name && m.MediaType == Media_Type && m.LanguageID == LanguageID).FirstOrDefault();
+                requestedMedia.TotalImmersionTimes += timeTaken;
+                LanguageDataContext.Update(requestedMedia);
+                return requestedMedia;
+            }
+            else
+            {
+                ImmersionMedia requestedMedia = new ImmersionMedia(Media_Name);
+                requestedMedia.MediaType = Media_Type;
+                requestedMedia.LanguageID = LanguageID;
+                requestedMedia.TotalImmersionTimes = timeTaken;
+                LanguageDataContext.Add(requestedMedia);
+                return requestedMedia;
+            }
         }
 
         private void CancelButton_Click(object sender, RoutedEventArgs e)
