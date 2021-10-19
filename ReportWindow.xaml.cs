8084f1a76703c2c308aa375070943dc5017f7fdb
quangkhoinguyen1995@gmail.com <quangkhoinguyen1995@gmail.com>
2021-10-19 23:25:09 +1300

    Setting up Report Window

--- ReportWindow.xaml.cs
+++ ReportWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using language_learning_tracker.Language_Data;
 
 namespace language_learning_tracker
 {
@@ -19,9 +20,27 @@ namespace language_learning_tracker
     /// </summary>
     public partial class ReportWindow : Window
     {
+        private List<LanguageList> Language_List;
+        private LanguageDataDbContext LanguageDataContext;
         public ReportWindow()
         {
             InitializeComponent();
+            LanguageDataContext = new LanguageDataDbContext();
+            Start_Date.SelectedDate = DateTime.Today;
+            End_Date.SelectedDate = DateTime.Today;
+            Language_List = LanguageDataContext.Language.ToList();
+            Language_Name.ItemsSource = Language_List.Select(s => s.LanguageName);
+        }
+
+        private void ViewReportButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Close the window
+            this.Close();
         }
     }
 }
